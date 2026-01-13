5e118b3d30a61ae956e75ed4ae5dd99090d947b0
leegolem <136658102+leegolem@users.noreply.github.com>
2026-01-13 14:38:32 +0900

    delete trash

--- programmers/lv1/1_두_수의_차.py
+++
@@ -1,9 +0,0 @@
-# 1_두_수의_차.py
-# 날짜: 2026-01-10
-# 문제: 두 수의 차
-# 난이도: Lv1
-# 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120803
-
-def solution(num1, num2):
-    answer = num1 - num2
-    return answer
